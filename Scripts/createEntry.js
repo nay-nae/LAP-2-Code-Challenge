b2a6c5f537c6a7eebd721f6a314c68e356dfc3e5
nay-nae <naomi_emojevbe@hotmail.com>
2022-04-20 12:53:34 +0100

    collection created for new posts

---
+++ Scripts/createEntry.js
@@ -0,0 +1,22 @@
+db.createCollection("new entry", {
+    validator: {
+        $jsonSchema:{
+            bsonType: "object",
+            required: ["title", "author", "story"],
+            properties: {
+                title: {
+                    basonType:"string",
+                    description: "must be a string and is required"
+                },
+                author: {
+                    basonType:"string",
+                    description: "must be a string"
+                },
+                story: {
+                    basonType:"string",
+                    description: "must be a string and is required"
+                },
+            }
+        }
+    }
+}
